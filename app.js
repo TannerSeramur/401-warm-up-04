d277b26f9453c0d5ac88adafce12fff50030eadc
Tanner Seramur <seramurt@gmail.com>
2019-01-04 11:08:57 -0800

    finished

--- app.js
+++ app.js
@@ -28,20 +28,23 @@ const superagent = require('superagent');
 
 // WITH ASYNC
 let fetchPeopleWithAsync = async () => {
+    try{
     let data = await superagent.get('https://swapi.co/api/people/');
     let people = (data.body && data.body.results) || [];
     let peopleReq = people.map((person) => {
         return superagent.get(person.url);
     });
-    let starwarsNames = await Promise.all(peopleRequests)
+    let starwarsNames = await Promise.all(peopleReq)
     .then(people => {
-        let name = [];
+        let names = [];
         for(let data of people){
             names.push(data.body.name);
         }
         return names;
     });
     return starwarsNames;
+}
+catch(e){console.error(e);}
 };
 
 fetchPeopleWithAsync()
